6a93a2cc0592c1d0cf9352ed49fec7bfa2a22e83
Dmitry Kovalkov <dmitriy.k@kultprosvet.net>
2025-09-26 12:28:56 +0300

    119. Добавление Формы

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,2 +1,21 @@
-const MealItemForm = (props) => {};
+import styles from './MealItemForm.module.css';
+import Input from '../../UI/Input';
+
+const MealItemForm = (props) => {
+  return (
+    <form className={styles.form}>
+      <Input
+        label="Количество"
+        input={{
+          id: 'amount',
+          type: 'number',
+          min: '1',
+          step: '1',
+          defaultValue: '1'
+        }}
+      />
+      <button>Добавить</button>
+    </form>
+  );
+};
 export default MealItemForm;
