32679d9040c6184c32139e210f9fb0d4300b8a56
Dmitry Kovalkov <dmitriy.k@kultprosvet.net>
2025-09-26 20:57:59 +0300

    125. Использование Контекста

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,22 @@
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+  const ctx = useContext(CartContext);
+
+  const cartItemsNumber = ctx.items.reduce((currentValue, item) => {
+    return currentValue + item.amount;
+  }, 0);
+
   return (
     <buttom className={styles.button} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Корзина</span>
-      <span className={styles.badge}>2</span>
+      <span className={styles.badge}>{cartItemsNumber}</span>
     </buttom>
   );
 };
