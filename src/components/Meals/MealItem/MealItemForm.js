4c8a9af1ec7b1354bc5332e997507e98714c7c26
Dmitry Kovalkov <dmitriy.k@kultprosvet.net>
2025-09-26 21:59:36 +0300

    127. Использование refs

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,33 @@
 import styles from './MealItemForm.module.css';
 import Input from '../../UI/Input';
+import { useRef, useState } from 'react';
 
 const MealItemForm = (props) => {
+  const [isAmountValid, setAmountValid] = useState(true);
+
+  const amountInputRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const inputAmount = amountInputRef.current.value;
+
+    if (
+      inputAmount.trim().length === 0 ||
+      +inputAmount < 1 ||
+      +inputAmount > 10
+    ) {
+      setAmountValid(false);
+      return;
+    }
+
+    props.onAddToCart(+inputAmount);
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Количество"
         input={{
           id: props.id,
@@ -15,6 +38,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>Добавить</button>
+      {!isAmountValid && <p>Пожалуйста введите количество от 1 до 10</p>}
     </form>
   );
 };
