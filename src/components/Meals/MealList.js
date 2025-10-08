ac145e47a5207dfe5e62ff658b62130c9c87d308
Dmitry Kovalkov <dmitriy.k@kultprosvet.net>
2025-10-08 20:10:08 +0300

    193. Управление Состоянием Загрузки

--- src/components/Meals/MealList.js
+++ src/components/Meals/MealList.js
@@ -5,9 +5,11 @@ import { useEffect, useState } from 'react';
 
 const MealList = (props) => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const getMeals = async () => {
+      setIsLoading(true);
       const res = await fetch(
         'https://react-cours-http-3ee12-default-rtdb.firebaseio.com/meals.json'
       );
@@ -24,10 +26,19 @@ const MealList = (props) => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
     getMeals();
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={styles.loading}>
+        <p>Извлечение данных с сервера...</p>
+      </section>
+    );
+  }
 
   const mealList = meals.map((meal) => (
     <MealItem
